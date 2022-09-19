24bc96feadf70a8c00e000071c38f7e4c8fda1f4
Aw3K <dedkam1@wp.pl>
2022-09-19 15:11:38 +0200

    usunięcie starego pomysłu

--- biuropodrozy/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.biuropodrozy {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.biuropodrozy to javafx.fxml;
-    exports com.example.biuropodrozy;
-}
