dc4aed8e7b87ce90979cf03fee0c7155b9ffaf73
XewoniX <xewonix36@gmail.com>
2022-09-19 16:56:08 +0200

    poprawa gui

--- UMK_Biuro/src/main/java/com/example/umk_biuro/HelloApplication.java
+++ UMK_Biuro/src/main/java/com/example/umk_biuro/HelloApplication.java
@@ -1,19 +1,10 @@
 package com.example.umk_biuro;
 
-
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-import java.util.Date;
 import java.io.IOException;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.util.Properties;
 
 public class HelloApplication extends Application {
     @Override
